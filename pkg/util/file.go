4a629e9b36b36c1940d4c44e68df8dd6e2f2ec04
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-19 08:59:25 +0530

    util.Join -> filepath.Join

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -9,10 +9,6 @@ func PathLen(path string) int {
 	return strings.Count(path, string(os.PathSeparator))
 }
 
-func Join(fileName string, dir string) string {
-	return dir + string(os.PathSeparator) + fileName
-}
-
 func Exists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
