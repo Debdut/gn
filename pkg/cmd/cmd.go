485073b91afb9cfc0f5cf9b8d0523fb94338f06c
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-19 12:34:17 +0530

    command structs

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -1,1 +1,34 @@
 package cmd
+
+type ArgCheck func(*Command, []string) bool
+type ArgRun func(*Command, []string) error
+
+type Arg struct {
+	Args  []string
+	Check ArgCheck
+	Run   ArgRun
+}
+
+type CommandExample struct {
+	Command string
+	Output  string
+}
+
+type CommandMeta struct {
+	Short    string // Short Description
+	Long     string // Long Description
+	Aliases  []string
+	Examples []CommandExample
+}
+
+type CommandRun func(*Command, []string) error
+
+type Command struct {
+	Name        string
+	SubCommands [](*Command)
+	Parent      *Command
+	Args        [](*Arg)
+	Modifiers   []string
+	Meta        CommandMeta
+	Run         CommandRun
+}
