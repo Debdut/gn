dc8f6a485251112396e20e53ffecc61cc00e2fee
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-19 13:22:24 +0530

    commands and args receivers

--- pkg/command/arg.go
+++ pkg/command/arg.go
@@ -4,7 +4,23 @@ type ArgCheck func(*Command, []string) bool
 type ArgRun func(*Command, []string) error
 
 type Arg struct {
-	Args  []string
-	Check ArgCheck
-	Run   ArgRun
+	Args []string
+
+	Modifiers []string
+	Parent    *Command
+	Check     ArgCheck
+	Run       ArgRun
+
+	Short    string // Short Description
+	Long     string // Long Description
+	Aliases  []string
+	Examples []string
+}
+
+// Adds args to this command
+func (c *Command) AddArgs(args ...*Arg) {
+	for _, arg := range args {
+		arg.Parent = c
+		c.Args = append(c.Args, arg)
+	}
 }
--- pkg/command/command.go
+++ pkg/command/command.go
@@ -5,21 +5,38 @@ type CommandExample struct {
 	Output  string
 }
 
-type CommandMeta struct {
+type CommandRun func(*Command, []string) error
+
+type Command struct {
+	Name string
+
+	SubCommands [](*Command)
+	Parent      *Command
+	Args        [](*Arg)
+	Modifiers   []string
+	Run         CommandRun
+
 	Short    string // Short Description
 	Long     string // Long Description
 	Aliases  []string
 	Examples []CommandExample
 }
 
-type CommandRun func(*Command, []string) error
+// Returns the path of the command
+func (c *Command) Path() string {
+	if c.Parent != nil {
+		return c.Parent.Path() + " " + c.Name
+	}
+	return c.Name
+}
 
-type Command struct {
-	Name        string
-	SubCommands [](*Command)
-	Parent      *Command
-	Args        [](*Arg)
-	Modifiers   []string
-	Meta        CommandMeta
-	Run         CommandRun
+// Adds sub commands to this command
+func (c *Command) AddCommand(cmds ...*Command) {
+	for _, cmd := range cmds {
+		if cmd == c {
+			panic("Command can't be a child of itself")
+		}
+		cmd.Parent = c
+		c.SubCommands = append(c.SubCommands, cmd)
+	}
 }
