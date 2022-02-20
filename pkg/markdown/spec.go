eaddae6c45c556bc79b9b318f9af3f3e1a882c03
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-20 09:38:51 +0530

    markdown spec

---
+++ pkg/markdown/spec.go
@@ -0,0 +1,65 @@
+package markdown
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Heading struct {
+	Text  *Text
+	Level uint8
+}
+
+func (h *Heading) Render() string {
+	return fmt.Sprintf(
+		"%s %s",
+		strings.Repeat("#", int(h.Level)),
+		String(h.Text),
+	)
+}
+
+type ListItem struct {
+	Text  *Text
+	List  *[]ListItem
+	Order bool
+}
+
+func (l *ListItem) Render(prefix string, level uint8) string {
+	text := ""
+	if l.Text != nil {
+		text = fmt.Sprintf(
+			"%s %s %s",
+			strings.Repeat("  ", int(level)),
+			prefix,
+			String(l.Text),
+		)
+	}
+	if len(*l.List) > 0 {
+		text += "\n"
+		for i, li := range *l.List {
+			childPrefix := "*"
+			if l.Order {
+				childPrefix = fmt.Sprint(i + 1)
+			}
+			text += li.Render(childPrefix, level+1)
+		}
+	}
+	return text
+}
+
+type List struct {
+	List  *[]ListItem
+	Order bool
+}
+
+func (l *List) Render() string {
+	text := ""
+	for i, li := range *l.List {
+		prefix := "*"
+		if l.Order {
+			prefix = fmt.Sprint(i + 1)
+		}
+		text += li.Render(prefix, 1) + "\n"
+	}
+	return text
+}
