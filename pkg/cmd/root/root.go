1f0d2815e3f72ebf0a22ba341360442be359018a
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-20 08:05:02 +0530

    root Command

--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -1,1 +1,25 @@
 package root
+
+import (
+	"github.com/debdut/gn/pkg/command"
+
+	"github.com/MakeNowJust/heredoc"
+)
+
+func New() *command.Command {
+	cmd := &command.Command{
+		Name:    "Generator",
+		Command: "gn",
+		Usage:   "gn <command> <subcommand> ... :modifier:modifier:",
+
+		Short: "The Next React Scaffolder & Generator",
+		Long: heredoc.Doc(`
+			A Code Generator and Scaffolding tool for the JS, TS ecosystem
+			Scaffold your whole project, and generate components, files,
+			file groups in your project for frameworks like Next, React.
+			Never write boilerplate or repetative code again!
+		`),
+	}
+
+	return cmd
+}
