a88d93a4e6e8709f992bd3dae561724859bdfed5
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-27 11:48:07 +0530

    add next, react to root

--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -1,9 +1,11 @@
 package root
 
 import (
+	"github.com/MakeNowJust/heredoc"
 	"github.com/debdut/gn/pkg/command"
 
-	"github.com/MakeNowJust/heredoc"
+	"github.com/debdut/gn/pkg/cmd/next"
+	"github.com/debdut/gn/pkg/cmd/react"
 )
 
 func New() *command.Command {
@@ -11,15 +13,19 @@ func New() *command.Command {
 		Name:    "Generator",
 		Command: "gn",
 		Use:     "gn <command> <subcommand> ... :modifier:modifier:",
-
-		Short: "The Next React Scaffolder & Generator",
+		Aliases: []string{"generate"},
+		Short:   "The Next React Scaffolder & Generator",
 		Long: heredoc.Doc(`
 			A Code Generator and Scaffolding tool for the JS, TS ecosystem
 			Scaffold your whole project, and generate components, files,
 			file groups in your project for frameworks like Next, React.
 			Never write boilerplate or repetative code again!
 		`),
+		SubCommands: []*command.Command{},
 	}
+
+	cmd.SubCommands = append(cmd.SubCommands, react.New())
+	cmd.SubCommands = append(cmd.SubCommands, next.New())
 
 	return cmd
 }
