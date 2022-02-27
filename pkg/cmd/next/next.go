9a04d93fd392493b5f3d4102c88e1231d79e2e47
Debdut Karmakar <iawaiponly@gmail.com>
2022-02-27 11:47:43 +0530

    next command

--- pkg/cmd/next/next.go
+++ pkg/cmd/next/next.go
@@ -1,1 +1,23 @@
 package next
+
+import (
+	"github.com/debdut/gn/pkg/command"
+
+	"github.com/MakeNowJust/heredoc"
+)
+
+func New() *command.Command {
+	cmd := &command.Command{
+		Name:    "Next",
+		Command: "next",
+		Use:     "gn next <subcommand> ... :modifier:modifier:",
+		Aliases: []string{"nxt"},
+		Short:   "Generate templates for Next",
+		Long: heredoc.Doc(`
+			Generate templates for Next pages, components, routes, apis, hooks,
+			also next libraries such as redux. Scaffold your whole next project.
+		`),
+	}
+
+	return cmd
+}
