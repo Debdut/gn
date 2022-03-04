a93f6cdb9d52c4e6db748ede116788a4f1ab7861
Debdut Karmakar <iawaiponly@gmail.com>
2022-03-04 22:10:50 +0530

    command parse

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/debdut/gn/pkg/cmd/root"
 	"github.com/debdut/gn/pkg/command"
@@ -9,11 +11,18 @@ import (
 
 func main() {
 	rootCmd := root.New()
-	// fmt.Println(rootCmd.Help())
-
-	matches := rootCmd.Match([]string{"nxt", "p", "crt"})
-	for _, a := range matches {
-		cmd := a.(*command.Command)
-		fmt.Printf("%s\n", cmd.Path())
+	args := os.Args[1:]
+	matches := rootCmd.Match(args)
+	if len(matches) == 0 {
+		log.Fatalln("command not found")
+	} else if len(matches) == 1 {
+		cmd := matches[0].(*command.Command)
+		fmt.Println(cmd.Help())
+	} else {
+		fmt.Println("multiples matches found")
+		for _, a := range matches {
+			cmd := a.(*command.Command)
+			fmt.Println(cmd.Path())
+		}
 	}
 }
